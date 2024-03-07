b9d230a0e78cc89ca50d2b7f07dcddd09dfb0215
João Gomes[ <jf.gomes458@gmail.com>
2024-03-06 20:26:48 -0400

    added post schema

---
+++ src/models/Posts.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    tag: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    createdBy: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+export default mongoose.model('Posts', userSchema)
